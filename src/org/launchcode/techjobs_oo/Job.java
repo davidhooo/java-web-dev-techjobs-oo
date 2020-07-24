4bada9506c9e83b5d3b3e893179c23661834622c
David Ho <davidfromcali@gmail.com>
2020-07-23 23:45:18 -0400

    finished up the tests

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -36,16 +36,16 @@ public class Job {
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
-        return value;
-    }
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
 
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
         if (this == o) return true;
         if (!(o instanceof Job)) return false;
-        Job job = (Job) o;
+        Job job = (Job) o;    //casting!
         return this.id == job.id;
     }
 
--- src/test/JobTest.java
+++ src/test/JobTest.java
@@ -3,35 +3,76 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.OrderWith;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JobTest {
+    Job job1;
+    Job job2;
+    Job job3;
+    Job job4;
+    Job job5;
 
-    @Before
-    public void setup() {
-        Job job1 = new Job();
-        Job job2 = new Job();
-        Job job3 = new Job("Woodworker");
-    }
+//    Objects are being instantiated again after every test, throwing off ID numbers for testing,
+//    also JUNIT runs tests in random order which will also change ID numbers, i have begun just running these tests individually, only god can judge me
 
-    @Test //test first and second instantiation, ID = 1 and 2
-    public void emptyConstructor() {
-        assertEquals(1, job1.getId());
-        assertEquals(2, job2.getId());
-        assertEquals(3, job3.getId());
-        assertEquals("Woodworker", job3.getName());
-    }
+//    @Before
+//    public void setup() {
+//        job1 = new Job();
+//        job2 = new Job();
+//        job3 = new Job("Product tester",
+//                new Employer("ACME"),
+//                new Location("Desert"),
+//                new PositionType("Quality control"),
+//                new CoreCompetency("Persistence"));
+//    }
 
     @Test
     public void testSettingJobId() {
+        job1 = new Job();
+        job2 = new Job();
 
+        assertEquals(1, job1.getId());
+        assertEquals(2, job2.getId());
     }
 
     @Test
     public void testJobConstructorSetsAllFields() {
+        job3 = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
 
+        assertEquals(true, job3.getName() instanceof String);
+        assertEquals(true, job3.getEmployer() instanceof Employer);
+        assertEquals(true, job3.getLocation() instanceof Location);
+        assertEquals(true, job3.getPositionType() instanceof PositionType);
+        assertEquals(true, job3.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("Product tester", job3.getName());
+        assertEquals("ACME", job3.getEmployer().getValue());
+        assertEquals("Desert", job3.getLocation().getValue());
+        assertEquals("Quality control", job3.getPositionType().getValue());
+        assertEquals("Persistence", job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        job4 = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        job5 = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertEquals(false, job4.equals(job5));
     }
 }
 
